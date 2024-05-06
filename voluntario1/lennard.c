ca5ed5354f994918b34f371f45ca309515e72e03
jeesuspereez <jesuspergar34@gmail.com>
2024-05-06 21:08:11 +0200

    version medio estable pero siguen divergiendo negativamente

--- voluntario1/lennard.c
+++ voluntario1/lennard.c
@@ -1,31 +1,27 @@
-/* programa que simula la dinámica molecular de un gas con un potencial de lennard jones
--V(r)=4eps[(sigma/r)^12 - (sigma/r)^6]
--F(r)= -parcialV/r = 24eps*sigma^6(2sigma^6-r^6)/r^13 */  
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
 
-#define Argonmass (6.6335209 * pow(10, -26)) // Masa en kg
-#define Armstrong (1 * pow(10, -10)) // Armstrong en metros
-#define PI (3.1415926535897932384)
+#define Argonmass (6.6335209L * pow(10, -26)) // Masa en kg
+#define Armstrong (1 * pow(10, -10))          // Armstrong en metros
+#define PI (3.1415926535897932384L)
 
 // Declaración de funciones
-void rescm(double *masa, int n);
-void rescr(double *pos, int sig, int n);
-void corriger(double *pos, int sig, int n);
-void aceleracion(double *aceleracion, double *posx, double *posy, int L, int n);
-void calculopos(double *pos, double *vel, double *acel, double h, int n);
-void calculov(double *velocidad, double *aux, double *acel, double h, int n);
-void calculow(double *aux, double *vel, double *acel, double h, int n);
-double cinetica(double *masa, double *velx, double *vely, int n);
-double potencial(double *masa, double *posx, double *posy, double *acelx, double *acely, int n);
-double mangular(double *masa, double *posx, double *posy, double *velx, double *vely, int n);
-void generar_posiciones(double *posx, double *posy, int dimension, int longitud);
-void generar_velocidades(double *velx, double *vely, int dimension);
-void imprimirCoordenadas(double *x, double *y, int n);
-void contorno(double *pos, int tampart, int longitud);
+void rescm(long double *masa, int n);
+void rescr(long double *pos, int sig, int n);
+void corriger(long double *pos, int sig, int n);
+void aceleracion(long double *aceleracion, long double *posx, long double *posy, int L, int n);
+void calculopos(long double *pos, long double *vel, long double *acel, long double h, int n);
+void calculov(long double *velocidad, long double *aux, long double *acel, long double h, int n);
+void calculow(long double *aux, long double *vel, long double *acel, long double h, int n);
+long double cinetica(long double *masa, long double *velx, long double *vely, int n);
+long double potencial(long double *masa, long double *posx, long double *posy, long double *acelx, long double *acely, int n);
+long double mangular(long double *masa, long double *posx, long double *posy, long double *velx, long double *vely, int n);
+void generar_posiciones(long double *posx, long double *posy, int dimension, int longitud);
+void generar_velocidades(long double *velx, long double *vely, int dimension);
+void imprimirCoordenadas(long double *x, long double *y, int n);
+void contorno(long double *pos, int tampart, int longitud);
 
 int main(void)
 {
@@ -52,33 +48,32 @@ int main(void)
 
     // Definición de variables
     int i, filas, j, k, N, L, sigma, epsilon;
-    double *m, *r_x, *r_y, *v_x, *w_x, *w_y, *v_y, *a_x, *a_y, t, h;
+    long double *m, *r_x, *r_y, *v_x, *w_x, *w_y, *v_y, *a_x, *a_y, t, h;
 
     t = 0;
-    h=0.002; //paso
+    h = 0.0002; // paso
     sigma = 1;
     epsilon = 1;
 
- 
-    L = 10;  //tamaño de la caja
-    N = 20; //numero de particulas
-    filas=N; 
+    L = 10; // tamaño de la caja
+    N = 20; // numero de particulas
+    filas = N;
 
     // Asignando memoria para los vectores
-    m = (double *)malloc((filas) * sizeof(double));
-    r_x = (double *)malloc(filas * sizeof(double));
-    r_y = (double *)malloc(filas * sizeof(double));
-    v_x = (double *)malloc(filas * sizeof(double));
-    v_y = (double *)malloc(filas * sizeof(double));
-    w_x = (double *)malloc(filas * sizeof(double));
-    w_y = (double *)malloc(filas * sizeof(double));
-    a_x = (double *)malloc(filas * sizeof(double));
-    a_y = (double *)malloc(filas * sizeof(double));
+    m = (long double *)malloc((filas) * sizeof(long double));
+    r_x = (long double *)malloc(filas * sizeof(long double));
+    r_y = (long double *)malloc(filas * sizeof(long double));
+    v_x = (long double *)malloc(filas * sizeof(long double));
+    v_y = (long double *)malloc(filas * sizeof(long double));
+    w_x = (long double *)malloc(filas * sizeof(long double));
+    w_y = (long double *)malloc(filas * sizeof(long double));
+    a_x = (long double *)malloc(filas * sizeof(long double));
+    a_y = (long double *)malloc(filas * sizeof(long double));
 
     // Inicializando aceleraciones a cero
     for (i = 0; i < filas; i++)
     {
-        m[i]=0;
+        m[i] = 0;
         r_x[i] = 0;
         r_y[i] = 0;
         v_x[i] = 0;
@@ -87,11 +82,10 @@ int main(void)
         w_y[i] = 0;
         a_x[i] = 0;
         a_y[i] = 0;
-        
     }
 
     //asignamos posicion random
-     generar_posiciones(r_x, r_y, N, L);
+    generar_posiciones(r_x, r_y, N, L);
 
     // Rescalando vectores de posición
     rescr(r_x, sigma, filas);
@@ -106,21 +100,18 @@ int main(void)
     // Rescalamos masas
     rescm(m, filas);
 
-    generar_velocidades(v_x,v_y,filas);
+    generar_velocidades(v_x, v_y, filas);
 
     // Rescalando velocidades
-   /* rescr(v_y, sigma, filas);
+    /* rescr(v_y, sigma, filas);
     corriget(v_y, filas);*/
 
-   //mostramos vectores posiciones por pantalla a ver si esta bien
+    //mostramos vectores posiciones por pantalla a ver si esta bien
     imprimirCoordenadas(r_x, r_y, filas);
 
     // Calculando aceleraciones para la primera iteración
     aceleracion(a_x, r_x, r_y, L, filas);
-    aceleracion(a_y, r_y, r_x, L, filas); 
-
- 
-
+    aceleracion(a_y, r_y, r_x, L, filas);
 
     /* ALGORITMO DE VERLET */
     for (t = 0; t < 100; t++)
@@ -143,32 +134,32 @@ int main(void)
         aceleracion(a_y, r_y, r_x, L, filas);
 
         // Calculando nuevas velocidades
-        calculov(v_x , w_x, a_x, t, filas);
-        calculov(v_y , w_y, a_y, t, filas);
+        calculov(v_x, w_x, a_x, t, filas);
+        calculov(v_y, w_y, a_y, t, filas);
 
         // Corrigiendo posiciones para escribirlas en el archivo
         corriger(r_x, sigma, filas);
         corriger(r_y, sigma, filas);
 
         //imprimo posiciones nuevas
-         for(k=0; k<filas; k++)
+        for (k = 0; k < filas; k++)
         {
-            fprintf(archivo, "%.10f", r_x[k]);
-            fprintf(archivo, ", %.10f\n", r_y[k]);
+            fprintf(archivo, "%.10Lf", r_x[k]);
+            fprintf(archivo, ", %.10Lf\n", r_y[k]);
         }
         fprintf(archivo, "\n");
 
         // Calculando energía y momento angular total
-        double V = potencial(m, r_x, r_y, a_x, a_y, filas);
-        double cin = cinetica(m, v_x, v_y, filas);
-        double energiatotal =  cin + V;
-        double momentoang = mangular(m, r_x, r_y,  v_x, v_y, filas);
+        long double V = potencial(m, r_x, r_y, a_x, a_y, filas);
+        long double cin = cinetica(m, v_x, v_y, filas);
+        long double energiatotal = cin + V;
+        long double momentoang = mangular(m, r_x, r_y, v_x, v_y, filas);
 
         // Escribiendo en los archivos
-        fprintf(archivo_, "%.10f\n", energiatotal);
-        fprintf(archivo_cinetic, "%.10f\n", cin);
-        fprintf(archivo_potenciale, "%.10f\n", V);
-        fprintf(archivo__, "%.10f\n", momentoang);
+        fprintf(archivo_, "%.10Lf\n", energiatotal);
+        fprintf(archivo_cinetic, "%.10Lf\n", cin);
+        fprintf(archivo_potenciale, "%.10Lf\n", V);
+        fprintf(archivo__, "%.10Lf\n", momentoang);
     }
 
     // Liberando memoria asignada para los vectores
@@ -193,7 +184,7 @@ int main(void)
 }
 
 // Función que rescala las masas
-void rescm(double *masa, int n)
+void rescm(long double *masa, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -202,7 +193,7 @@ void rescm(double *masa, int n)
 }
 
 // Función que rescata los vectores de posición
-void rescr(double *pos, int sig ,int n)
+void rescr(long double *pos, int sig, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -211,7 +202,7 @@ void rescr(double *pos, int sig ,int n)
 }
 
 // Función que devuelve la escala original de r
-void corriger(double *pos, int sig, int n)
+void corriger(long double *pos, int sig, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -219,10 +210,9 @@ void corriger(double *pos, int sig, int n)
     }
 }
 
-
-void aceleracion(double *aceleracion, double *posx, double *posy, int L, int n)
+void aceleracion(long double *aceleracion, long double *posx, long double *posy, int L, int n)
 {
-    double distx, disty, distancia;
+    long double distx, disty, distancia;
     for (int i = 0; i < n; i++)
     {
         aceleracion[i] = 0;
@@ -235,47 +225,18 @@ void aceleracion(double *aceleracion, double *posx, double *posy, int L, int n)
             if (i != j)
             {
                 distx = posx[i] - posx[j];
-		        disty = posy[i] - posy[j];
-		   //     distx = distx - round(distx/(2*L))*(2.*L);
-		    //    disty = disty - round(disty/(2*L))*(2.*L);
+                disty = posy[i] - posy[j];
+               // distx = distx - round(distx / (2 * L)) * (2. * L);
+               // disty = disty - round(disty / (2 * L)) * (2. * L);
                 distancia = sqrt(distx * distx + disty * disty);
-                aceleracion[i] += ((posx[i] - posx[j])/distancia)*24 * (2.0 / pow(distancia, 13) - 1.0 / pow(distancia, 7));
+                aceleracion[i] += ((posx[i] - posx[j]) / distancia) * 24 * (2.0 / pow(distancia, 13) - 1.0 / pow(distancia, 7));
             }
         }
     }
 }
-
-
-/*
-// Función para calcular aceleración
-void aceleracion(double *aceleracion, double *posx, double *posy, int L, int n)
-{
-    double distx, disty, distancia;
-    for (int i = 0; i < n; i++)
-    {
-        aceleracion[i] = 0;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i != j)
-            {
-                distx = posx[i] - posx[j];
-		        disty = posy[i] - posy[j];
-		        distx = distx - round(distx/(2*L))*(2.*L);
-		        disty = disty - round(disty/(2*L))*(2.*L);
-                distancia = sqrt(distx * distx + disty * disty);
-                aceleracion[i] += 24 * (2.0 / pow(distancia, 13) - 1.0 / pow(distancia, 7));
-            }
-        }
-    }
-}
-*/
 
 // Funciones auxiliares para calcular velocidad
-void calculow(double *aux, double *vel, double *acel, double h, int n)
+void calculow(long double *aux, long double *vel, long double *acel, long double h, int n)
 {
     for (int i = 1; i < n; i++)
     {
@@ -283,7 +244,7 @@ void calculow(double *aux, double *vel, double *acel, double h, int n)
     }
 }
 
-void calculov(double *velocidad, double *aux, double *acel, double h, int n)
+void calculov(long double *velocidad, long double *aux, long double *acel, long double h, int n)
 {
     for (int i = 1; i < n; i++)
     {
@@ -292,7 +253,7 @@ void calculov(double *velocidad, double *aux, double *acel, double h, int n)
 }
 
 // Función para calcular la nueva posición
-void calculopos(double *pos, double *vel, double *acel, double h, int n)
+void calculopos(long double *pos, long double *vel, long double *acel, long double h, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -301,9 +262,9 @@ void calculopos(double *pos, double *vel, double *acel, double h, int n)
 }
 
 // Función para calcular el momento angular
-double mangular(double *masa, double *posx, double *posy, double *velx, double *vely, int n)
+long double mangular(long double *masa, long double *posx, long double *posy, long double *velx, long double *vely, int n)
 {
-    double L = 0;
+    long double L = 0;
     for (int i = 1; i < n; i++)
     {
         L += masa[i] * sqrt(pow(posx[i], 2) + pow(posy[i], 2)) * sqrt(pow(velx[i], 2) + pow(vely[i], 2));
@@ -312,9 +273,9 @@ double mangular(double *masa, double *posx, double *posy, double *velx, double *
 }
 
 // Función para calcular la energía cinética total
-double cinetica(double *masa, double *velx, double *vely, int n)
+long double cinetica(long double *masa, long double *velx, long double *vely, int n)
 {
-    double kinetic = 0;
+    long double kinetic = 0;
     for (int i = 1; i < n; i++)
     {
         kinetic += 0.5 * masa[i] * (velx[i] * velx[i] + vely[i] * vely[i]);
@@ -323,9 +284,9 @@ double cinetica(double *masa, double *velx, double *vely, int n)
 }
 
 // Función para calcular la energía potencial total
-double potencial(double *masa, double *posx, double *posy, double *acelx, double *acely, int n)
+long double potencial(long double *masa, long double *posx, long double *posy, long double *acelx, long double *acely, int n)
 {
-    double pot = 0;
+    long double pot = 0;
     for (int i = 1; i < n; i++)
     {
         pot += -masa[i] * sqrt(pow(posx[i], 2) + pow(posy[i], 2)) * sqrt(pow(acelx[i], 2) + pow(acely[i], 2));
@@ -333,123 +294,74 @@ double potencial(double *masa, double *posx, double *posy, double *acelx, double
     return pot;
 }
 
-void generar_posiciones(double *posx, double *posy, int dimension, int longitud)
-{   
-    int n,m, i, j;
+void generar_posiciones(long double *posx, long double *posy, int dimension, int longitud)
+{
+    int n, m, i, j;
 
-    for(i = 0; i < dimension; i++) {
-        do {
-            posx[i] = ((double)rand() / RAND_MAX) * (longitud - 2) + 1;
-            posy[i] = ((double)rand() / RAND_MAX) * (longitud - 2) + 1;
+    for (i = 0; i < dimension; i++)
+    {
+        do
+        {
+            posx[i] = ((long double)rand() / RAND_MAX) * (longitud - 2) + 1;
+            posy[i] = ((long double)rand() / RAND_MAX) * (longitud - 2) + 1;
 
-            for(j = 0; j < i; j++) {
-                double distancia_x = posx[i] - posx[j];
-                double distancia_y = posy[i] - posy[j];
-                double distancia_entre_particulas = sqrt(distancia_x * distancia_x + distancia_y * distancia_y);
-                if(distancia_entre_particulas < 1.5) {
+            for (j = 0; j < i; j++)
+            {
+                long double distancia_x = posx[i] - posx[j];
+                long double distancia_y = posy[i] - posy[j];
+                long double distancia_entre_particulas = sqrt(distancia_x * distancia_x + distancia_y * distancia_y);
+                if (distancia_entre_particulas < 1.7)
+                {
                     break;
                 }
             }
-        } while(j < i);
+        } while (j < i);
     }
 }
 
-/*
-void generar_posiciones(double *pos_x, double *pos_y, int dimension, int tamano_red)
- {
-    // Número máximo de celdas en la red cuadrada
-    int num_celdas = tamano_red * tamano_red;
-
-    // Verificar si hay suficientes celdas para todas las partículas
-    if (num_celdas < dimension) {
-        printf("Error: No hay suficientes celdas en la red para almacenar todas las partículas.\n");
-        return;
-    }
-
-    // Asignar posiciones aleatorias
-    int ocupado[num_celdas]; // Array para verificar si una celda está ocupada
-    for (int i = 0; i < num_celdas; i++) {
-        ocupado[i] = 0; // Inicializar todas las celdas como no ocupadas
-    }
-
-    // Generar posiciones aleatorias para cada partícula
-    for (int i = 0; i < dimension; i++) {
-        int celda;
-
-        // Buscar una celda no ocupada
-        do {
-            celda = rand() % num_celdas;
-        } while (ocupado[celda]);
-
-        // Marcar la celda como ocupada
-        ocupado[celda] = 1;
-
-        // Calcular las coordenadas (x, y) de la celda
-        int fila = celda / tamano_red;
-        int columna = celda % tamano_red;
-
-        // Asignar la posición de la partícula
-        pos_x[i] = (double)columna + ((double)rand() / RAND_MAX); // Posición x aleatoria entre columna y columna+1
-        pos_y[i] = (double)fila + ((double)rand() / RAND_MAX);    // Posición y aleatoria entre fila y fila+1
-    }
-}
-*/
-
-/*void generar_posiciones(double *pos_x, double *pos_y, int dimension, int tamano_red)
-{
-int i;
-    for(i=0;i<dimension;i++){
-    pos_x[i]=i+1;
-    pos_y[i]=0;
-    }
-}*/
-
-void generar_velocidades(double *velx, double *vely, int dimension)
+void generar_velocidades(long double *velx, long double *vely, int dimension)
 {
     int valor_aleatorio, valor_aleatorio2, i;
-    double theta, r;
-    r=1.;
+    long double theta, r;
+    r = 1.;
 
-    for(i=0; i<dimension; i++){
-    theta=2.*PI*rand()/RAND_MAX;
-    velx[i] = r*cos(theta); 
-    vely[i]= r*sin(theta);
+    for (i = 0; i < dimension; i++)
+    {
+        theta = 2. * PI * rand() / RAND_MAX;
+        velx[i] = r * cos(theta);
+        vely[i] = r * sin(theta);
     }
 }
 
 // Función para imprimir las coordenadas (x, y) como una matriz
-void imprimirCoordenadas(double *x, double *y, int n)
- {
+void imprimirCoordenadas(long double *x, long double *y, int n)
+{
     // Imprimir las coordenadas como una matriz
     printf("Coordenadas:\n");
-    for (int i = 0; i < n; i++) {
-        printf("(%.2f, %.2f)\n", x[i], y[i]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("(%.2Lf, %.2Lf)\n", x[i], y[i]);
     }
 }
 
-void contorno(double *pos, int tampart, int longitud)
- {
-int i, k;
+void contorno(long double *pos, int tampart, int longitud)
+{
+    int i, k;
 
-    for(i=0; i< tampart; i++)
-    {        
-        k=0;
-        if (pos[i]>longitud)
+    for (i = 0; i < tampart; i++)
+    {
+        k = 0;
+        if (pos[i] > longitud)
         {
-            k=floor(pos[i]/longitud); //la funcion floor devuelve el resto real de la fraccion por ej pos=2.5L --> floor(pos/long)=.5
-            pos[i]= pos[i] - k*longitud;
+            k = floor(pos[i] / longitud); //la funcion floor devuelve el resto real de la fraccion por ej pos=2.5L --> floor(pos/long)=.5
+            pos[i] = pos[i] - k * longitud;
         }
-            else if(pos[i]<0){
-            k= floor(-pos[i]/longitud);
-     //       pos[i]= pos[i] + longitud -k*longitud;
-      // pos[i]=  longitud -k;
-      //pos[i] - k*longitud
-       pos[i]= longitud +pos[i] + k*longitud;
-
-            }
-        else pos[i]=pos[i];
+        else if (pos[i] < 0)
+        {
+            k = floor(-pos[i] / longitud);
+            pos[i] = longitud + pos[i] + k * longitud;
+        }
+        else
+            pos[i] = pos[i];
     }
-
- }
-
-//se podria hacer una funcion para ver la distancia al eje x y al eje y y compararla con la distancia entre particulas y quedarnos con la menor.
+}
